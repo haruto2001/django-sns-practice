e5035821626176807d6eeb4c13895b593769b88d
haruto2001 <tarako5515@gmail.com>
2022-10-29 17:25:38 +0900

    feat: add FOLLOW button

--- snsapp/urls.py
+++ snsapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Home, MyPost, DetailPost, CreatePost, UpdatePost, DeletePost, LikeHome, LikeDetail, FollowHome, FollowDetail
+from .views import Home, MyPost, DetailPost, CreatePost, UpdatePost, DeletePost, LikeHome, LikeDetail, FollowHome, FollowDetail, FollowList
 
 
 urlpatterns = [
@@ -13,4 +13,5 @@ urlpatterns = [
     path('like-detail/<int:pk>', LikeDetail.as_view(), name='like-detail'),
     path('follow-home/<int:pk>', FollowHome.as_view(), name='follow-home'),
     path('follow-detail/<int:pk>', FollowDetail.as_view(), name='follow-detail'),
+    path('follow-list/', FollowList.as_view(), name='follow-list'),
 ]
