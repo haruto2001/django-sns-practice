d73b8a9b0097a5ca7aaaec48117d4852ba7e4ac7
haruto2001 <tarako5515@gmail.com>
2022-11-08 17:38:06 +0900

    feat: add an image field to Post

--- snsapp/models.py
+++ snsapp/models.py
@@ -1,10 +1,18 @@
+import uuid
+
 from django.db import models
 from django.contrib.auth.models import User
+
+
+def image_directory_path(instance, filename):
+    """ユニークなIDを生成して元のファイル名の拡張子を結合"""
+    return f'images/{str(uuid.uuid4())}.{filename.split('.')[-1]}'
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    image = models.ImageField(upload_to=image_directory_path, blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     like = models.ManyToManyField(User, related_name='related_post', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
