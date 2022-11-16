bd7eb8b2ca9c0b0a99bcf3548266118bc031cf6d
haruto2001 <tarako5515@gmail.com>
2022-11-16 20:31:22 +0900

    refactor: improve readability of browser-back-control.js

--- static/js/browser-back-control.js
+++ static/js/browser-back-control.js
@@ -1,12 +1,12 @@
 // 新規投稿ページ，投稿編集ページ，コメント作成ページ以外のページでブラウザバックを禁止
 // ※Google Chromeではフォーカスが一度でも当たっていないとブラウザバックできてしまう
-if !( document.URL.match(create) || document.URL.match(update) ){
-  window.onload = function() {
+window.onload = function() {
+  if ( !document.URL.match('create') && !document.URL.match('update') ) {
     history.pushState(null, null, null);
 
     window.addEventListener("popstate", function (e) {
       history.pushState(null, null, null);
       return;
     });
-  };
-}
+  }
+};
