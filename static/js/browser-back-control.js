9a7bcd852f2576b47928bc3fc0ae89f9bc5fdf7b
haruto2001 <tarako5515@gmail.com>
2022-11-16 19:27:56 +0900

    fix: disable to browser back on all pages

--- static/js/browser-back-control.js
+++ static/js/browser-back-control.js
@@ -1,10 +1,10 @@
-// 直前に閲覧していたページのURLの末尾を取得
-var ref = document.referrer;
-var stringArray = ref.split('/');
-// 編集画面から詳細画面に遷移した場合のみブラウザバックを禁止
-if ( stringArray[stringArray.length-1] == 'update' ) {
-  history.pushState(null, null, location.href);
-  window.addEventListener('popstate', (e) => {
-    history.go(1);
-  })
+// ブラウザバックを禁止
+// ※Google Chromeではフォーカスが一度でも当たっていないとブラウザバックできてしまう
+window.onload = function() {
+  history.pushState(null, null, null);
+
+  window.addEventListener("popstate", function (e) {
+    history.pushState(null, null, null);
+    return;
+  });
 };
