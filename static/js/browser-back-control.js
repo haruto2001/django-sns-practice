5c7a6cc9786c896d439a8ff3eb04d33d086dd624
haruto2001 <tarako5515@gmail.com>
2022-11-16 19:44:56 +0900

    fix: fix a bug where the cancel buttons don't respond

--- static/js/browser-back-control.js
+++ static/js/browser-back-control.js
@@ -1,10 +1,12 @@
-// ブラウザバックを禁止
+// 新規投稿ページ，投稿編集ページ，コメント作成ページ以外のページでブラウザバックを禁止
 // ※Google Chromeではフォーカスが一度でも当たっていないとブラウザバックできてしまう
-window.onload = function() {
-  history.pushState(null, null, null);
-
-  window.addEventListener("popstate", function (e) {
+if !( document.URL.match(create) || document.URL.match(update) ){
+  window.onload = function() {
     history.pushState(null, null, null);
-    return;
-  });
-};
+
+    window.addEventListener("popstate", function (e) {
+      history.pushState(null, null, null);
+      return;
+    });
+  };
+}
